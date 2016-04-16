f0112fb1445bac4e322884933715e9cc506684c8
Lisa VanOrdenKaan <lmvanorden@gmail.com>
2016-04-16 15:55:08 -0400

    second

--- main.js
+++ main.js
@@ -1,19 +1,71 @@
-var PartyBus = require('./party_bus.js');
+var prompt = prompt('request');
+var Word = require('./word.js')
 
-//fun fact: Beersville is a real city in PA!!!
-var zbtPartyBus = new PartyBus('Cosmo', 'NYC', 'Beersville, PA');
+//call the .start method on prompt
+game.startGame();
 
-//none
-console.log('dudes?', zbtPartyBus.duds);
+var game = function(game) {
+	wordBank: ['sunshine', 'beach', 'volleyball'];
+	guessesRemaining: 10;
+	currentWrd: null; 
+    this.word = wrd;
 
-zbtPartyBus.addDud('female', 'Moon', 10);
+var zbtWord = new Word('sunshine');
+currentWrd = zbtWord;
 
-console.log('dudes?', zbtPartyBus.duds);
+.getLets function(currentWrd) {
 
-zbtPartyBus.addDud('male', 'Breaker', 10);
-zbtPartyBus.addDud('female', 'Pine', 8);
-zbtPartyBus.addDud('male', 'Chance', 9);
-zbtPartyBus.addDud('male', 'West', 4);
-zbtPartyBus.addDud('female', 'Honor', 6);
+	keepPromptingUser() {	
+	  self = keepPromptingUser;
+	}
+}
 
-console.log('dudes?', zbtPartyBus.duds);
+client.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+
+prompt.get('u', 'h', 's','n', 'i', 'e', );  {
+	guessLetter =['i']
+
+	anonymous function(err, result)) {
+		if (!error) {
+    console.log("The Letter or space you guessed is " + result.guessed)
+	}
+}
+
+findHowManyOfUserGuess = currentWrd.checkIfLetterFound(result.guessLetter);
+
+
+if findHowManyOfUserGuess = 0 {
+		console.log("You guessed wrong~!" - guessesRemaining)
+	} else {
+		console.log("You guessed right")
+		if function currentWrd.didWeFindTheWord() returns true
+			console.log('You Won!!!')
+
+			return 1 to the end of the game
+			console.log("guessesRemaining: " + value of guessesRemaining);
+			console.log("call the wordRender() mthod on the currentWrd )
+	} else {
+
+	}
+}
+	if (guessesRemaining > 0 && currentWrd.found == 'false') {
+	keepPromptingUser()
+	}else{
+		if the guessesRemaining = 0 {
+			console.log("Game over bro", this.Word)
+		}else{
+			console.log(wordRender(currentWrd))
+		}
+	}
+game.startGame();
+
+
+
+
+
+module.exports = Letter;
